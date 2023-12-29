b61a14e83245266be9b977de3dc0a411afd21b4b
lolloleone93 <leone.lorenzo@hotmail.it>
2023-12-29 20:18:46 +0100

    Prova finale

--- Git_Commit.bat
+++ Git_Commit.bat
@@ -1,8 +1,17 @@
 @echo off
 
 REM Imposta le nuove credenziali
+echo ----------------------------
+echo ----------------------------
+echo Credenziali commit:
+echo Nome Utente: lolloleone93
+echo Email: leone.lorenzo@hotmail.it
+echo ----------------------------
+echo ----------------------------
 git config user.name "lolloleone93"
 git config user.email "leone.lorenzo@hotmail.it"
+
+
 
 echo Inserisci il messaggio di commit:
 set /p commit_message=
@@ -10,17 +19,29 @@ set /p commit_message=
 git add .
 git commit -m "%commit_message%"
 
+echo ----------------------------
+echo ----------------------------
 echo Commit effettuato con successo!
+echo ----------------------------
+echo ----------------------------
 
 git push origin main
-
+echo ----------------------------
+echo ----------------------------
 echo Push sul repositary effettuato con successo!
+echo ----------------------------
+echo ----------------------------
 
 REM Ripristina le credenziali originali
 git config --unset user.name
 git config --unset user.email
 
+echo ----------------------------
+echo ----------------------------
+
 echo Credenziali ripristinate.
+echo ----------------------------
+echo ----------------------------
 
 REM Salva le credenziali originali
 set "original_username="
@@ -30,6 +51,7 @@ set "original_email="
 for /f "delims=" %%G in ('git config user.email') do set "original_email=%%G"
 
 REM Visualizza le credenziali originali
+echo ----------------------------
 echo Credenziali originali:
 echo Nome Utente: %original_username%
 echo Email: %original_email%
