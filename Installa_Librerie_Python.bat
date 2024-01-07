47acda9e4d98aaaace2ce8d2aebd1ec9eccf568e
lolloleone93 <leone.lorenzo@hotmail.it>
2024-01-07 17:57:30 +0100

    aggiunto installa librerie python

---
+++ Installa_Librerie_Python.bat
@@ -0,0 +1,17 @@
+@echo off
+
+:: Lista delle librerie da installare
+set librerie=tkinter pandas os sys re datetime shutil time tqdm
+
+:: Percorso dell'interprete Python
+set python_interpreter=python
+
+:: Itera attraverso la lista delle librerie e installale con pip
+for %%i in (%librerie%) do (
+    %python_interpreter% -m pip install %%i
+)
+
+:: Messaggio di completamento
+echo Librerie installate con successo.
+pause
+
--- frame.py
+++ frame.py
@@ -1,25 +1,36 @@
 import tkinter as tk
-from tkinter import ttk
 
-import tkinter as tk
-from tkinter import ttk
+def mostra_prove():
+    print("Mostra Prove")
 
-def on_combobox_change(event):
-    frame.pack_forget()  # Nasconde il frame temporaneamente
-    # Aggiungi qui il codice da eseguire quando l'elemento del Combobox viene selezionato
-    root.update()  # Aggiorna la finestra principale per rimuovere l'effetto di evidenziazione
-    frame.pack()  # Mostra nuovamente il frame
+def clear_list_box():
+    print("Cancella Listbox")
 
 root = tk.Tk()
+root.title("Esempio con Frames Separati")
 
-frame = ttk.Frame(root)
-frame.pack(pady=10)
+# Frame per la colonna 0
+frame_colonna_0 = tk.Frame(root)
+frame_colonna_0.pack(side="left", padx=10)
 
-# Crea un Combobox all'interno del frame
-combobox = ttk.Combobox(frame, values=["Elemento 1", "Elemento 2", "Elemento 3"])
-combobox.pack()
+# Listbox nella colonna 0
+list_box = tk.Listbox(frame_colonna_0, selectmode=tk.MULTIPLE, width=20, height=5)
+list_box.grid(row=0, column=0, pady=10)
 
-# Aggiungi un bind per gestire l'evento di cambio selezione nel Combobox
-combobox.bind("<<ComboboxSelected>>", on_combobox_change)
+# Frame per la colonna 1
+frame_colonna_1 = tk.Frame(root)
+frame_colonna_1.pack(side="left", padx=10)
+
+# Button "Mostra Prove" nella colonna 1
+mostra_prove_button = tk.Button(frame_colonna_1, text="Mostra Prove", width=15, height=2, command=mostra_prove)
+mostra_prove_button.grid(row=0, column=0, pady=10)
+
+# Frame per la colonna 2
+frame_colonna_2 = tk.Frame(root)
+frame_colonna_2.pack(side="left", padx=10)
+
+# Button "Cancella" nella colonna 2
+clear_listbox = tk.Button(frame_colonna_2, text="Cancella", width=15, height=2, command=clear_list_box)
+clear_listbox.grid(row=0, column=0, pady=10)
 
 root.mainloop()
