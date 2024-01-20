15b49f7172e409e6d0c1d8fe49a7a7bc9569ef65
lolloleone93 <leone.lorenzo@hotmail.it>
2024-01-20 13:29:36 +0100

    aggiunta cartella linux

---
+++ Git_Commit_Linux.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo Credenziali commit:
+echo Nome Utente: lolloleone93
+echo Email: leone.lorenzo@hotmail.it
+echo ----------------------------
+echo ----------------------------
+git config user.name "lolloleone93"
+git config user.email "leone.lorenzo@hotmail.it"
+
+read -p "Inserisci il messaggio del commit: " commit_message
+
+# Aggiungi tutti i file modificati al commit
+git add .
+
+# Esegui il commit con il messaggio specificato
+git commit -m "$commit_message"
+
+echo "Commit eseguito con successo."
+
